2a91b8c6140fee9361aae52ecd720648cb93ae95
David Adolfo Garcia Giron <Sagitario@TecsupMacBook.local>
2019-04-06 15:03:27 -0500

    first commit

---
+++ ej1_lab1/miPrimerApp/ejemplo 2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var x1, x2, a, b, c:Double!
+
+// Valores para ABC
+
+a = 10
+b = 115
+c = 20
+
+x1 = (-b+(sqrt(pow(b, 2)-(4*a*c))))/(2*a)
+x2 = (-b-(sqrt(pow(b, 2)-(4*a*c))))/(2*a)
+
+print("Resultado de Operacion Nº1: \(x1!)")
+print("Resultado de Operacion Nº1: \(x2!)")
+
+
